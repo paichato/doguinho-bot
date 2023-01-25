586b8b6de277898a1cc2576df5f81fc0863ed18c
paichato <tsenane14@gmail.com>
2023-01-25 10:01:08 +0200

    feat: inverted order

--- bot.js
+++ bot.js
@@ -7,15 +7,6 @@ var cors = require("cors");
 var useragent = require("express-useragent");
 const port = process.env.PORT || 3001;
 
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(useragent.express());
-
-app.listen(port, () => {
-  console.log("🚀SERVER STARTED ON PORT 3333..");
-});
-
 venom
   .create("session-name", (base64Qr, asciiQR) => {
     console.log(asciiQR);
@@ -23,57 +14,66 @@ venom
   })
   .then((client) => {
     start(client);
-
-    app.post("/code", async (req, res) => {
-      const { msg, phonenumber } = req.body;
-
-      try {
-        if (!msg) {
-          return res.send("Erro: parametros em falta");
-        }
-
-        if (!client.isConnected) {
-          return res.send(
-            "Erro: cliente ainda nao conectou, tente novamente mais tarde."
-          );
-        }
-
-        if (msg.toLowerCase().includes("code")) {
-          const code = msg.split(",")[1];
-          await client
-            .sendText(`${phonenumber}@c.us`, `O seu código OTP é:*${code}*`)
-            .then((result) => {
-              console.log(result);
-              if (result.status !== 200) {
-                return res.send(result.text);
-              }
-              res.send("code sent");
-            })
-            .catch((erro) => {
-              console.log(erro ?? "erro ao enviar mensagem");
-              res.send(erro ?? "erro ao enviar mensagem");
-            });
-        }
-        if (msg.toLowerCase().startsWith("/contacts")) {
-          await client
-            .getAllChatsContacts()
-            .then((result) => {
-              return res.send(result);
-            })
-            .catch((err) => {
-              res.send(err);
-            });
-        }
-      } catch (error) {
-        res.send(error);
-      }
-    });
   })
   .catch((erro) => {
-    console.log(erro);
+    console.log(erro ?? "erro");
   });
 
 async function start(client) {
+  const app = express();
+  app.use(cors());
+  app.use(express.json());
+  app.use(useragent.express());
+
+  app.listen(port, () => {
+    console.log("🚀SERVER STARTED ON PORT 3333..");
+  });
+
+  app.post("/code", async (req, res) => {
+    const { msg, phonenumber } = req.body;
+
+    try {
+      if (!msg) {
+        return res.send("Erro: parametros em falta");
+      }
+
+      if (!client.isConnected) {
+        return res.send(
+          "Erro: cliente ainda nao conectou, tente novamente mais tarde."
+        );
+      }
+
+      if (msg.toLowerCase().includes("code")) {
+        const code = msg.split(",")[1];
+        await client
+          .sendText(`${phonenumber}@c.us`, `O seu código OTP é:*${code}*`)
+          .then((result) => {
+            console.log(result);
+            if (result.status !== 200) {
+              return res.send(result.text);
+            }
+            res.send("code sent");
+          })
+          .catch((erro) => {
+            console.log(erro ?? "erro ao enviar mensagem");
+            res.send(erro ?? "erro ao enviar mensagem");
+          });
+      }
+      if (msg.toLowerCase().startsWith("/contacts")) {
+        await client
+          .getAllChatsContacts()
+          .then((result) => {
+            return res.send(result);
+          })
+          .catch((err) => {
+            res.send(err);
+          });
+      }
+    } catch (error) {
+      res.send(error);
+    }
+  });
+
   client.onMessage((message) => {
     if (message.body === "Ola" && message.isGroupMsg === false) {
       client
